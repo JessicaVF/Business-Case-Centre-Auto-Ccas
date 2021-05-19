e449682081000d3ca694056f73bc8c767e33b02a
JessicaVF <villarjessica@hotmail.com>
2021-05-19 16:20:50 +0200

    safety commit, making front end of annonce detail

--- angularVersion/ccas/src/app/annonce.service.ts
+++ angularVersion/ccas/src/app/annonce.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Annonce } from './models/annonce';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { isLoweredSymbol } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,17 @@ export class AnnonceService {
 annonce!: Annonce[];
 dummyAnnonce: Annonce[] = [
   {reference: 0,
-    titre:" string",
+    titre:"Cadillac",
     aneeMiseCirculation: 2000,
-    marque:" string",
-    modele:" string",
-    carburant:" string",
+    marque:"Cadillac",
+    modele:"Escalade",
+    carburant:"Diesel",
     kilometrage: 2000,
-    descriptionComplete:" string",
-    descriptionCourt:" string",
+    descriptionComplete: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra hendrerit est, eu eleifend nisl ornare ut. Etiam pharetra sit amet sem id pretium. Nunc a mauris nisi. Nam ullamcorper, nisl vitae malesuada congue, ante ex consectetur enim, et gravida nisl lacus vitae mi. Donec quis congue nisl. Vestibulum eget pretium dolor. Donec semper justo in odio vulputate fermentum",
+    descriptionCourt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra hendrerit est, eu eleifend nisl ornare ut.",
     prix: 2000,
     datePublication: new Date,
-    photos:["../../assets/cadillac_1_preview.jpg", "dummyPhoto", "dummyPhoto", "dummyPhoto", "dummyPhoto"]
+    photos:["../../assets/cadillac_1_preview.jpg", "../../assets/cadillac(1)", "../../assets/cadillac(2)", "../../assets/cadillac(3)", "dummyPhoto"]
   },
   {reference: 1,
     titre:" string 2",
