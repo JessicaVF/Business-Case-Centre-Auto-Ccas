b92cf4021502384dca422790b829011c1112f20f
JessicaVF <villarjessica@hotmail.com>
2021-09-04 16:26:23 +0200

    add announce ready

--- angularVersion/ccas/src/app/my-profile/my-profile.component.ts
+++ angularVersion/ccas/src/app/my-profile/my-profile.component.ts
@@ -1,4 +1,7 @@
+import { LogicalFileSystem } from '@angular/compiler-cli/src/ngtsc/file_system';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AnnonceService } from '../annonce.service';
 import { User } from '../models/user.model';
 import { UserService } from '../user.service';
 
@@ -10,18 +13,53 @@ import { UserService } from '../user.service';
 export class MyProfileComponent implements OnInit {
 
   user!:User;
-  constructor(private userService: UserService) { }
+  makes: Array<any> =[];
+  models: Array<any>=[];
+  fuelTypes: Array<any>=[];
+  addAnnonceForm!: FormGroup;
+
+  constructor(private userService: UserService, private fb: FormBuilder, private annonceService : AnnonceService) {
+
+    this.addAnnonceForm = fb.group({
+      title: fb.control('', Validators.required),
+      description: fb.control('', Validators.required),
+      shortDescription: fb.control('', Validators.required),
+      circulationYear: fb.control('', Validators.required),
+      kilometers: fb.control('', Validators.required),
+      price: fb.control('', Validators.required),
+
+      photos: fb.control(['https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg'], Validators.required),
+
+      make: fb.control(false, Validators.required),
+      model: fb.control(false, Validators.required),
+      fuelType: fb.control(false, Validators.required),
+      user: fb.control('', Validators.required),
+      garage: fb.control('', Validators.required),
+
+
+    })
+  }
 
   ngOnInit(): void {
+    this.userService.getOne().subscribe
+    ( data => {
+      this.user = data;}
+    );
+    this.annonceService.getMakes().then( data => this.makes = data);
+    this.annonceService.getFuelTypes().then(data => this.fuelTypes = data);
 
-    this.userService.getOne().subscribe( data => {
-      this.user = data;
+  }
 
-      ;
-    } );
-    // this.userService.test().subscribe(data=> {
-    //   console.log(data);
-    // });
+  getModels():any{
+    this.annonceService.getModelsByMake(this.addAnnonceForm.value.make).then(data=>this.models = data);
+  }
+  submitForm(){
+    let formInfo = this.addAnnonceForm.value;
+    formInfo.user = this.user.id;
+    let dataReady = {"title": formInfo.title, "description":formInfo.description, "shortDescription":formInfo.shortDescription, "circulationYear": formInfo.circulationYear, "kilometers": formInfo.kilometers, "price": formInfo.price };
+    let dataToRetrieve = {"user":formInfo.user, "make": 3, "model": formInfo.model, "fuelType": formInfo.fuelType, "garage": formInfo.garage}
+    const data = [dataToRetrieve, dataReady];
+    this.annonceService.add(data).subscribe(r => location.reload);
   }
 
 }
