5a3f26943bb187834751e76a06a36b5e25c5a5d9
JessicaVF <villarjessica@hotmail.com>
2021-09-06 12:17:02 +0200

    Garage edit functional

--- angularVersion/ccas/src/app/garage.service.ts
+++ angularVersion/ccas/src/app/garage.service.ts
@@ -21,10 +21,10 @@ export class GarageService {
     return this.http.get<Garage[]>(this.link, {headers});
   }
 
-edit(garage:Garage, id:number){
-  const headers = { 'Authorization': "Bearer " + sessionStorage.getItem("token") };
+edit(infoToSend:any, id:number){
+  // const headers = { 'Authorization': "Bearer " + sessionStorage.getItem("token") };
   this.link = "http://127.0.0.1:8000/garage/edit/" + id;
-  return this.http.patch<Garage[]>(this.link, garage);
+  return this.http.patch<any[]>(this.link, infoToSend);
 }
 
 delete(id:number){
--- angularVersion/ccas/src/app/garage/garage.component.ts
+++ angularVersion/ccas/src/app/garage/garage.component.ts
@@ -10,8 +10,8 @@ import { Garage } from '../models/garage.model';
 })
 export class GarageComponent implements OnInit {
 
-  @Input() garage!:Garage;
-  editGarage!:number;
+  @Input() garage!: Garage;
+  editGarage!: number;
   editGarageForm!: FormGroup;
   editAddressForm!: FormGroup;
 
@@ -20,29 +20,34 @@ export class GarageComponent implements OnInit {
   ngOnInit(): void {
 
   }
-  edit(id:number){
+  edit(id: number) {
     this.editGarage = id;
 
     this.editGarageForm = this.fb.group({
       id: this.fb.control(this.garage.id, Validators.required),
       name: this.fb.control(this.garage.name, Validators.required),
       telephone: this.fb.control(this.garage.telephone, Validators.required),
-      user: this.fb.control(this.garage.user, Validators.required),
-      road: this.fb.control(this.garage.address.road),
-        number: this.fb.control(this.garage.address.number),
-        complement: this.fb.control(this.garage.address.complement),
-        codePostal: this.fb.control(this.garage.address.codePostal),
-        ville: this.fb.control(this.garage.address.ville)
+      addressId : this.fb.control(this.garage.address.id),
+      road: this.fb.control(this.garage.address.road, Validators.required),
+      number: this.fb.control(this.garage.address.number, Validators.required),
+      complement: this.fb.control(this.garage.address.complement, Validators.required),
+      codePostal: this.fb.control(this.garage.address.codePostal, Validators.required),
+      city: this.fb.control(this.garage.address.city, Validators.required)
     })
 
   }
-  submitForm(){
+  submitForm() {
     this.editGarage = 0;
     const formInfo = this.editGarageForm.value;
-    this.garageService.edit(formInfo, this.garage.id).subscribe(r => location.reload());
+    let addressInput = {"id": formInfo.addressId, "number": formInfo.number, "complement": formInfo.complement,
+                        "road": formInfo.road, "codePostal": formInfo.codePostal, "city": formInfo.city};
+    let garageInfo = {"name":formInfo.name, "telephone":formInfo.telephone};
+    let infoToSend = [addressInput, garageInfo];
+    this.garageService.edit(infoToSend, this.garage.id).subscribe(r => location.reload());
+
   }
-  delete(id:number){
-     this.garageService.delete(id).subscribe(r => location.reload());;
+  delete(id: number) {
+    this.garageService.delete(id).subscribe(r => location.reload());;
 
   }
 
