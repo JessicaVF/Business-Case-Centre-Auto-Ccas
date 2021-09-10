0fdac81d4078bf21e75d00dbc43757fa608ea17e
JessicaVF <villarjessica@hotmail.com>
2021-09-10 20:21:34 +0200

    add (1) photo in annonce (add annonce form)

--- angularVersion/ccas/src/app/add-annonce/add-annonce.component.ts
+++ angularVersion/ccas/src/app/add-annonce/add-annonce.component.ts
@@ -16,6 +16,7 @@ export class AddAnnonceComponent implements OnInit {
   models: Array<any>=[];
   fuelTypes: Array<any>=[];
   addAnnonceForm!: FormGroup;
+  imgURL!:any;
 
   constructor(private userService: UserService, private fb: FormBuilder, private annonceService : AnnonceService) {
     this.addAnnonceForm = fb.group({
@@ -26,7 +27,7 @@ export class AddAnnonceComponent implements OnInit {
       kilometers: fb.control('', Validators.required),
       price: fb.control('', Validators.required),
 
-      photos: fb.control(['https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg'], Validators.required),
+      photos: fb.control('', Validators.required),
 
       make: fb.control(false, Validators.required),
       model: fb.control(false, Validators.required),
@@ -49,10 +50,26 @@ export class AddAnnonceComponent implements OnInit {
   getModels():any{
     this.annonceService.getModelsByMake(this.addAnnonceForm.value.make).then(data=>this.models = data);
   }
+  test(event:any){
+
+    const file = event.target.files[0];
+    console.log("g");
+    console.log(file);
+    let reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload =(event2)=>{ this.imgURL = reader.result}
+    console.log(this.imgURL);
+
+  }
   submitForm(){
+
     let formInfo = this.addAnnonceForm.value;
+    let photos = [this.imgURL];
+    formInfo.photos = photos;
+
+
     formInfo.user = this.user.id;
-    let dataReady = {"title": formInfo.title, "description":formInfo.description, "shortDescription":formInfo.shortDescription, "circulationYear": formInfo.circulationYear, "kilometers": formInfo.kilometers, "price": formInfo.price };
+    let dataReady = {"title": formInfo.title, "description":formInfo.description, "shortDescription":formInfo.shortDescription, "circulationYear": formInfo.circulationYear, "kilometers": formInfo.kilometers, "price": formInfo.price, "photos": formInfo.photos };
     let dataToRetrieve = {"user":formInfo.user, "make": 3, "model": formInfo.model, "fuelType": formInfo.fuelType, "garage": formInfo.garage}
     const data = [dataToRetrieve, dataReady];
     this.annonceService.add(data).subscribe(r => location.reload());
