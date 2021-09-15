b78942e04bb060f8b156008384a749df46685371
JessicaVF <villarjessica@hotmail.com>
2021-09-15 15:30:35 +0200

    menu is dif for admin and for prf user when they visit "manage my account"

--- angularVersion/ccas/src/app/manage-my-account/manage-my-account.component.ts
+++ angularVersion/ccas/src/app/manage-my-account/manage-my-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 import { User } from '../models/user.model';
 import { UserService } from '../user.service';
 
@@ -9,11 +10,15 @@ import { UserService } from '../user.service';
 })
 export class ManageMyAccountComponent implements OnInit {
   user!:User;
-  constructor(private userService: UserService) { }
+  isAdmin!:any;
+  constructor(private userService: UserService, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.userService.getOne().subscribe( data => {
       this.user = data;});
+      this.isAdmin = this.authService.getIfAdminInStorage();
+      console.log(this.isAdmin);
+
   }
 
 }
